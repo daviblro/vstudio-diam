248d2fa9c6b999ff183cafb3cbc0eb43a9fc11d8
Guilherme <guilhermerico2004@gmail.com>
2025-03-13 09:54:36 +0000

    Já sei o useState, tudo a funcionar, acho eu.

--- semana4/src/App.js
+++ semana4/src/App.js
@@ -4,35 +4,24 @@ import Artista from "./Artista";
 import Artistas from "./Artistas";
 
 function App() {
-  const artistaAlinea1 = [//Nao tem urlvideo.
-    {
-      nome: "Artista",
-      imagem: "/images/6.jpg",
-      estilo: "Pop",
-      descricao:"Descrição: Algo sobre o Festival.",
-      data: "2021-07-30",
-      hora: "21:00",
-      urlvideo: "https://www.youtube.com/embed/zbs06__3378",
-    },
-
-  ];
-
   return (
     <>
       <Header />
-      <p style={{ textAlign: "center", fontSize: "30px", fontWeight: "bold" }}>Alínea 1</p>
-      {artistaAlinea1.map((artista) => (
-        <Artista
-          nome={artista.nome}
-          imagem={artista.imagem}
-          estilo={artista.estilo}
-          descricao={artista.descricao}
-          data={artista.data}
-          hora={artista.hora}
-          urlvideo={artista.urlvideo}
-        />
-      ))}
-      <h3 style={{ textAlign: "center", fontSize: "30px", fontWeight: "bold" }}>Alínea 3</h3>
+      <p style={{ textAlign: "center", fontSize: "30px", fontWeight: "bold" }}>
+        Alínea 1
+      </p>
+      <Artista
+        nome="ArtistaNome"
+        imagem="/images/6.jpg"
+        estilo="Pop"
+        descricao="Descrição: Algo sobre o Festival."
+        data="2021-07-30"
+        hora="21:00"
+        urlvideo="https://www.youtube.com/embed/zbs06__3378"
+      />
+      <h3 style={{ textAlign: "center", fontSize: "30px", fontWeight: "bold" }}>
+        Alínea 3
+      </h3>
 
       {Artistas.map((artista) => (
         <Artista
@@ -45,7 +34,6 @@ function App() {
           urlvideo={artista.urlvideo}
         />
       ))}
-
     </>
   );
 }
--- semana4/src/Artista.js
+++ semana4/src/Artista.js
@@ -1,4 +1,5 @@
 import React from "react";
+ 
 //----------------------------------Alínea 1--------------------------------
 /*
 @props: nome, imagem, estilo, descricao, urlvideo, data e hora.
@@ -8,12 +9,23 @@ import React from "react";
 //Falta abrir o vídeo no youtube on click na imagem
 
 function Artista(props) {
-  const [showVideo, setShowVideo] = React.useState(false);
+  /*const [showVideo, setShowVideo] = React.useState(false);
   
   const openyt = () => {
     setShowVideo(!showVideo);
   };
-  
+  */
+  function handleClick(nome) {
+    
+    const video = document.getElementById(nome);
+    if(video.style.display === "none"){
+      video.style.display = "block";
+    }
+    else{
+      video.style.display = "none";
+    }
+  }
+
   return (
     <>
       <div className="Artista">
@@ -25,23 +37,18 @@ function Artista(props) {
           src={props.imagem}
           alt={props.nome}
           className="TopicoApresentacao"
-          onClick={() => openyt(props.urlvideo)}
+          onClick={() => handleClick(props.nome)} 
           style={{ width: "50%", height: "50%" }}
-        ></img>
-        {showVideo && (
-          <div id="AparecerVideo">
-            <iframe
-              width="560"
-              height="315"
-              src={props.urlvideo} // Passando a URL do vídeo
-              frameBorder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          </div>
-        )}
+        ></img>  
+          <iframe id={props.nome} style={{display: "none"}}
+            width="560"
+            height="315"
+            src={props.urlvideo} // Passando a URL do vídeo
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
         <p className="TopicoApresentacao">Estilo Musical: {props.estilo}</p>
-        <section className="TopicoApresentacao Descricao">
+        <section className="TopicoApresentacao De</section>scricao">
           {props.descricao}
         </section>
       </div>
