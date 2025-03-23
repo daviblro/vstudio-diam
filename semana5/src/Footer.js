44fec7492b1ab7e3a9ce679036bc9448b91a491c
Guilherme <guilhermerico2004@gmail.com>
2025-03-23 17:26:02 +0000

    Final Ex5

--- semana5/src/Footer.js
+++ semana5/src/Footer.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Footer.css";
 
-function Footer() {
+function Footer() {//seria melhor em vez de mudar opacidade, eliminar e adicionar os elementos, assim grantiamos a centralização
   const [isVisible, setIsVisible] = React.useState(true);
 
   return (
     <>
       <footer onClick={() => setIsVisible(!isVisible)} className="footer">
-        <div className={`${isVisible ? "opacity-1" : "opacity-0"} div`}>
+        {/* <div className={`${!isVisible ? "opacity-1" : "opacity-0"} div  `}>Versão Original ....</div> */}
+        <div className={`${isVisible ? "opacity-1" : "opacity-0"} div `}>
           Davi Balieiro
           <br />
           Guilherme Riço
--- semana5/src/Header.js
+++ semana5/src/Header.js
@@ -7,21 +7,11 @@ function Header() {
       <div className="Header">
         Exercício da semana 4
         <div style={{ display: "flex", gap: "10px" }}>
-          <Link
-            to="/"
-            style={{ opacity: 1 }}
-            onMouseEnter={(e) => (e.target.style.opacity = 0.7)}
-            onMouseLeave={(e) => (e.target.style.opacity = 1)}
-          >
-            Home
+          <Link to="/">
+            <button className="btn">Home</button>
           </Link>
-          <Link
-            to="/FormVoluntario"
-            style={{ opacity: 1 }}
-            onMouseEnter={(e) => (e.target.style.opacity = 0.7)}
-            onMouseLeave={(e) => (e.target.style.opacity = 1)}
-          >
-            Formulário de Voluntários
+          <Link to="/FormVoluntario">
+            <button className="btn">Formulário de Voluntários</button>
           </Link>
         </div>
       </div>
