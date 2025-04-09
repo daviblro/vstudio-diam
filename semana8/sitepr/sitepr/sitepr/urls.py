9d0606420dc8d163c7ce3e08507018fe47713acc
Zhu8690 <luisjizhu@gmail.com>
2025-04-09 16:14:29 +0100

    compnoents

--- semana8/sitepr/sitepr/sitepr/urls.py
+++ semana8/sitepr/sitepr/sitepr/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from . import views
+app_name = 'votacao'
 urlpatterns = [
-    path('votacao/', include('votacao.urls')),  
-    path('admin/', admin.site.urls),
+ path('api/questions/', views.questions),
+ path('api/question/<int:question_id>', views.question_detail),
+ path('api/options/<int:question_id>', views.options),
+ path('api/option/<int:option_id>', views.option_detail),
 ]
